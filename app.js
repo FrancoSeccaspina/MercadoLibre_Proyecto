941e8ef05e805f787eb2f56556d169c00a1a9bbf
Franco Seccaspina <francosec2002@hotmail.com>
2022-07-20 17:51:33 -0300

    inicio proyecto

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.resolve(__dirname,'./public')));
+
+app.listen(port,() => console.log('Servidor corriendo...'));
+
+app.get('/', (req,res) =>{
+    res.sendFile( path.join(__dirname, './views/index.html'))
+})
+
+app.get('/register', (req,res) =>{
+    res.sendFile( path.join(__dirname, "views","register.html"))
+})
+
+app.get('/login', (req,res) =>{
+    res.sendFile( path.join(__dirname, './views/login.html'))
+})
